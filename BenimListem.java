cd18648ce075d628283979447595909e267cce13
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-08-05 13:25:26 +0300

    Add files via upload

---
+++ BenimListem.java
@@ -0,0 +1,155 @@
+package listeSınıfı;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BenimListem<T> {
+	private Object[] dizi;
+    private int boyut;
+    private static final int BASLANGIC_BOYUTU = 10;
+    
+    public BenimListem() {
+        this.dizi = new Object[BASLANGIC_BOYUTU];
+        this.boyut = 0;
+    }
+    
+    public BenimListem(int kapasite) {
+        this.dizi = new Object[kapasite];
+        this.boyut = 0;
+    }
+    
+    public int boyut() {
+        return boyut;
+    }
+    
+    public int kapasite() {
+        return dizi.length;
+    }
+    
+    public void ekle(T veri) {
+        if (boyut == dizi.length) {
+            kapasiteyiArtir();
+        }
+        dizi[boyut] = veri;
+        boyut++;
+    }
+    
+    public T getir(int indeks) {
+        if (indeks < 0 || indeks >= boyut) {
+            throw new IndexOutOfBoundsException("Geçersiz indeks: " + indeks);
+        }
+        return (T) dizi[indeks];
+    }
+    
+    private void kapasiteyiArtir() {
+        int yeniKapasite = dizi.length * 2;
+        Object[] yeniDizi = new Object[yeniKapasite];
+        System.arraycopy(dizi, 0, yeniDizi, 0, boyut);
+        dizi = yeniDizi;
+    }
+    
+    public T get(int indeks) {
+        if (indeks < 0 || indeks >= boyut) {
+            return null;
+        }
+        return (T) dizi[indeks];
+    }
+    
+    public T remove(int indeks) {
+        if (indeks < 0 || indeks >= boyut) {
+            return null;
+        }
+
+        T silinenVeri = (T) dizi[indeks];
+
+        
+        for (int i = indeks; i < boyut - 1; i++) {
+            dizi[i] = dizi[i + 1];
+        }
+
+        
+        dizi[boyut - 1] = null;
+        boyut--;
+
+        return silinenVeri;
+    }
+    
+    public T set(int indeks, T veri) {
+        if (indeks < 0 || indeks >= boyut) {
+            return null;
+        }
+
+        T eskiVeri = (T) dizi[indeks];
+        dizi[indeks] = veri;
+        return eskiVeri;
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < boyut; i++) {
+            sb.append(dizi[i]);
+            if (i < boyut - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    
+    public int indexOf(T veri) {
+        for (int i = 0; i < boyut; i++) {
+            if (veri.equals(dizi[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public int lastIndexOf(T veri) {
+        for (int i = boyut - 1; i >= 0; i--) {
+            if (veri.equals(dizi[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public boolean isEmpty() {
+        return boyut == 0;
+    }
+    
+    public T[] toArray(String[] strings) {
+        T[] array = (T[]) new Object[boyut];
+        for (int i = 0; i < boyut; i++) {
+            array[i] = (T) dizi[i];
+        }
+        return array;
+    }
+    
+    public void clear() {
+        Arrays.fill(dizi, null);
+        boyut = 0;
+    }
+    
+    public BenimListem<T> sublist(int start, int finish) {
+        if (start < 0 || finish >= boyut || start > finish) {
+            throw new IllegalArgumentException("Geçersiz indeks aralığı: " + start + " - " + finish);
+        }
+
+        BenimListem<T> altListe = new BenimListem<>(finish - start + 1);
+        for (int i = start; i <= finish; i++) {
+            altListe.ekle((T) dizi[i]);
+        }
+
+        return altListe;
+    }
+    
+    public boolean contains(T veri) {
+        return indexOf(veri) != -1;
+    }
+    
+    
+}
+
+
